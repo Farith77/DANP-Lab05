bdbcced5aef25346beabde07ed2020fbd0e0d154
yeyderjhjl <jturpoan.@unsa.edu.pe>
2026-06-16 11:41:00 -0500

    agregación de koin a detalle de producto

--- app/src/main/java/com/danp/lab5/ui/screens/product_detail/ProductDetailViewModel.kt
+++ app/src/main/java/com/danp/lab5/ui/screens/product_detail/ProductDetailViewModel.kt
@@ -1,13 +1,17 @@
 package com.danp.lab5.ui.screens.product_detail
 
 import androidx.lifecycle.ViewModel
+import com.danp.lab5.ProductLogger
 import com.danp.lab5.data.repository.ProductRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class ProductDetailViewModel(private val productRepository: ProductRepository) : ViewModel() {
+class ProductDetailViewModel(
+    private val productRepository: ProductRepository,
+    private val logger: ProductLogger
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(ProductDetailUiState())
     val uiState: StateFlow<ProductDetailUiState> = _uiState.asStateFlow()
@@ -15,6 +19,9 @@ class ProductDetailViewModel(private val productRepository: ProductRepository) :
     fun loadProduct(productId: Int, isInCart: Boolean) {
         _uiState.update { it.copy(isLoading = true) }
         val product = productRepository.getProductById(productId)
+        if (product != null) {
+            logger.logVisit(product.id.toString())
+        }
         _uiState.update { 
             it.copy(
                 product = product,
